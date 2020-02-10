0fcea920690ba00bf993969d86d1490f5fabbc62
randychoi95 <jhc130833@naver.com>
2020-02-10 10:29:53 +0900

    수정된 내용입니다.

--- app/src/main/java/com/ezen/mygithubtest/MainActivity.java
+++ app/src/main/java/com/ezen/mygithubtest/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // 소스를 수정했습니다.
+    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
